f4ac2788bb3835345fdf415c2b438fb6ad859c8f
Champlain <champlainmarius20@gmail.com>
2020-12-01 16:11:48 +0100

    Initialisation du projet

---
+++ src/main/java/com/bixterprise/damier/DamierApplication.java
@@ -0,0 +1,13 @@
+package com.bixterprise.damier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bixterprise/damier/DamierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bixterprise.damier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
